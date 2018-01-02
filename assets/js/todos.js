85182a820ba3444142ca942163f42090e7931ee2
Zac Duvall <duvallzac@gmail.com>
2018-01-02 15:56:32 -0700

    Complete functionality [add + remove]

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,24 @@
-
-$("li").on("click", function(){
+// Line-through on click
+$("ul").on("click", "li", function(){
     $(this).toggleClass("complete")
-    console.log("Yay!")
-    console.log("Github test Log")
 })
 
 // Click on 'X' to delete Todo
-$("span").on("click", function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(300, function(){
-        $(this).parent.remove();
+        // Below, 'this' refers to the whole .parent() - 
+        // which, in this case, is the whole <li> as well as the <span>
+        $(this).remove();
     });
     event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        // Grabbing new text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        //Create a new li and add to ul
+        $("ul").append("<li class='unselectable'><span>X</span> " + todoText + "</li>");
+    }
 })
